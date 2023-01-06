fc4e1cb60eb8095ee8d0151c4dd83b6fbcdd6882
Anisha0906 <anishadas880@gmail.com>
2023-01-06 09:18:58 +0530

    [Anisha]Add:Perfect Number

--- Program.cs
+++ Program.cs
@@ -11,13 +11,17 @@ namespace LogicalProgramming
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Problems ");
-            Console.WriteLine("Enter your choice 1.Fibonacci Series ");
+            Console.WriteLine("Enter your choice 1.Fibonacci Series 2.Perfect Number ");
             int selection = Convert.ToInt32(Console.ReadLine());
             switch (selection)
             {
                 case 1:
                     FibbonacciSeries FibboSeries = new FibbonacciSeries();
                     FibboSeries.CalculateFibonacciSeries();
+                    break;
+                case 2:
+                    PerfectNumber PerfectNum = new PerfectNumber();
+                    PerfectNum.CheckPerfectNumber();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection");
