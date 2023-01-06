5ba569ce31a578ff8e368190855046322e85f40f
Anisha0906 <anishadas880@gmail.com>
2023-01-06 18:36:17 +0530

    [Anisha]Add:Reverse a Number

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace LogicalProgramming
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Problems ");
-            Console.WriteLine("Enter your choice 1.Fibonacci Series 2.Perfect Number 3.Prime Number ");
+            Console.WriteLine("Enter your choice 1.Fibonacci Series 2.Perfect Number 3.Prime Number 4.Reverse Number ");
             int selection = Convert.ToInt32(Console.ReadLine());
             switch (selection)
             {
@@ -26,6 +26,10 @@ namespace LogicalProgramming
                 case 3:
                     PrimeNo Primenumber = new PrimeNo();
                     Primenumber.CheckPrimeNumber();
+                    break;
+                case 4:
+                    ReverseNumber reverseNum = new ReverseNumber();
+                    reverseNum.Reversenumber();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection");
---
+++ ReverseNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    class ReverseNumber
+    {
+        public int Number;
+        public int Reverse;
+        public int Remainder;
+        public void Reversenumber()
+        {
+            Console.WriteLine("Enter a No. to reverse");
+            Number = Convert.ToInt32(Console.ReadLine());
+            Reverse = 0;
+            while (Number > 0)
+            {
+                Remainder = Number % 10;
+                Reverse = (Reverse * 10) + Remainder;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Reverse No. is " + Reverse);
+            Console.ReadLine();
+        }
+    }
+}
