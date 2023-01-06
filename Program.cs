4abaac794478ed1e8b97e24677cb12f56a30b42f
Anisha0906 <anishadas880@gmail.com>
2023-01-06 09:04:44 +0530

    [Anisha]Add:

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Problems ");
+            Console.WriteLine("Enter your choice 1.Fibonacci Series ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    FibbonacciSeries FibboSeries = new FibbonacciSeries();
+                    FibboSeries.CalculateFibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
